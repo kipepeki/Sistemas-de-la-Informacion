8ad4375775fce3e25a030d1418848901c7651243
kipepeki <f.mondejar.2020@alumnos.urjc.es>
2023-03-26 10:09:08 +0200

    Iniciando ejercicio 2

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 import sqlite3
 import pandas as pd
 
@@ -27,9 +28,29 @@ cur.execute('''CREATE TABLE IF NOT EXISTS analisis (
 )''')
 
 ## Creamos tabla ALERTAS con el propio pandas ya que el formato de los datos permiten crearla directamente (primera fila del .csv)
-alerts = pd.read_csv("./data/alerts.csv")
-alerts.to_sql("alertas", con, if_exists="replace", index=False)
+alertas = pd.read_csv("./data/alerts.csv")
+alertas.to_sql("alertas", con, if_exists="replace", index=False)
+
+## Cogemos los datos de devices.json
+with open("./data/devices.json", "r") as file:
+    dispositivos = json.load(file)
+
+## Insertamos los datos en las tablas (en la tabla ALERTAS ya tenemos los datos introducidos)
+analisis_id = 0
+for dispositivo in dispositivos:
+    responsable = dispositivo['responsable']
+    cur.execute("INSERT OR IGNORE INTO responsables (nombre, telefono, rol) VALUES (?, ?, ?)", (responsable['nombre'], responsable['telefono'], responsable['rol']))
+    analisis = dispositivo['analisis']
+    if analisis["puertos_abiertos"] == 'None':
+        n_puertos_abiertos = 0
+    else:
+        n_puertos_abiertos = len(analisis["puertos_abiertos"])
+    cur.execute("INSERT INTO analisis (puertos_abiertos, n_puertos_abiertos, servicios, servicios_inseguros, vulnerabilidades_detectadas) VALUES (?, ?, ?, ?, ?)", (json.dumps(analisis['puertos_abiertos']), n_puertos_abiertos, analisis['servicios'], analisis['servicios_inseguros'], analisis['vulnerabilidades_detectadas']))
+    cur.execute("INSERT INTO dispositivos (id, ip, localizacion, responsable, analisis) VALUES (?, ?, ?, ?, ?)", (dispositivo['id'], dispositivo['ip'], dispositivo['localizacion'], responsable['nombre'], analisis_id))
+    analisis_id += 1
+
+
+### EJERCICIO 2 ###
 
 con.commit()
 con.close()
-
